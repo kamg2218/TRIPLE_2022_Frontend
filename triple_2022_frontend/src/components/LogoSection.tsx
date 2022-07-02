7a8818831317df177e97ab09a0f057ea8a6a6031
윤현지 <hyoon@student.42seoul.kr>
2022-07-03 02:09:48 +0900

    :white_check_mark: update LogoSection

--- triple_2022_frontend/src/components/LogoSection.tsx
+++ triple_2022_frontend/src/components/LogoSection.tsx
@@ -1,5 +1,28 @@
+import { styled } from '@stitches/react'
+
+import logo from '../img/triple2x.png'
+
+const LogoItems = styled('div')
+const LogoText = styled('div', {
+  bottom: '50px',
+  fontSize: '15px',
+  left: '50%',
+  position: 'absolute',
+  transform: 'translateX(-50%)',
+})
+const LogoImg = styled('img', {
+  display: 'block',
+  height: '338px',
+  width: '400px',
+})
+
 const LogoSection = () => {
-  return <>Logo</>
+  return (
+    <LogoItems>
+      <LogoImg src={logo} alt="logo" />
+      <LogoText>2021년 12월 기준</LogoText>
+    </LogoItems>
+  )
 }
 
 export default LogoSection
