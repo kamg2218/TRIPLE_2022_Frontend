911b76ab84ded1e60dffa4f97576dfaa36fa41ba
윤현지 <hyoon@student.42seoul.kr>
2022-07-03 02:12:55 +0900

    :white_check_mark: update TextSection

--- triple_2022_frontend/src/components/TextSection.tsx
+++ triple_2022_frontend/src/components/TextSection.tsx
@@ -1,5 +1,39 @@
+import { styled } from '@stitches/react'
+
+const TextItems = styled('div', {
+  backgroundColor: 'white',
+  marginLeft: '50%',
+})
+const TextItem = styled('div', {
+  marginBottom: '20px',
+})
+const TextBold = styled('span', {
+  fontWeight: 'bold',
+})
+
 const TextSection = () => {
-  return <>Text</>
+  return (
+    <TextItems>
+      <TextItem>
+        <TextBold>
+          <span id="traveler">0</span>만 명
+        </TextBold>
+        의 여행자
+      </TextItem>
+      <TextItem>
+        <TextBold>
+          <span id="review">0</span>만 개
+        </TextBold>
+        의 여행 리뷰
+      </TextItem>
+      <TextItem>
+        <TextBold>
+          <span id="schedule">0</span>만 개
+        </TextBold>
+        의 여행 일정
+      </TextItem>
+    </TextItems>
+  )
 }
 
 export default TextSection
