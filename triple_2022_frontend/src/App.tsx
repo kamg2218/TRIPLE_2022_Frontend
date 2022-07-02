2797b6588d9f134f17f91518b1ef2a1e84b778d8
윤현지 <hyoon@student.42seoul.kr>
2022-07-03 02:02:02 +0900

    :sparkles: create StatisticSection component

--- triple_2022_frontend/src/App.tsx
+++ triple_2022_frontend/src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <div className="App">APP</div>
-}
+import { styled } from '@stitches/react'
 
+import StatisticSection from './components/StatisticSection'
+
+const AppContent = styled('div', {
+  width: '100vw',
+  height: '100vh',
+  minWidth: '1200px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+const MainContent = styled('div', {
+  minHeight: '450px',
+  width: '100%',
+})
+
+function App() {
+  return (
+    <AppContent>
+      <MainContent>
+        <StatisticSection />
+      </MainContent>
+    </AppContent>
+  )
+}
 export default App
---
+++ triple_2022_frontend/src/components/StatisticSection.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@stitches/react'
+
+const StatisticItems = styled('div', {
+  margin: 'auto',
+  position: 'relative',
+  width: '1080px' /* 90% */,
+})
+
+const StatisticSection = () => {
+  return <StatisticItems>Statistic</StatisticItems>
+}
+
+export default StatisticSection
