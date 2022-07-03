84b854043ba1c7478583263b7f2e924657ad1cbc
윤현지 <hyoon@student.42seoul.kr>
2022-07-04 01:34:45 +0900

    :memo: update .eslintrc

--- triple_2022_frontend/.eslintrc.js
+++ triple_2022_frontend/.eslintrc.js
@@ -1,19 +1,3 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
 module.exports = createConfig({ type: 'frontend' })
-
-// const { extends: extendConfigs, overrides } = createConfig({ type: 'node' })
-
-// module.exports = {
-//   extends: [
-//     ...extendConfigs,
-//     // 확장할 규칙 이름...
-//   ],
-//   overrides: [
-//     ...overrides,
-//     // 특정 파일 대상 규칙...
-//   ],
-//   rules: {
-//     // 개별 규칙
-//   },
-// }
